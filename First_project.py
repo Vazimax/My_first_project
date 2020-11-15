e3001d5391706082b621feb36c342626ab306206
unknown <aboubakr.elhabti@gmail.com>
2020-11-15 20:58:39 +0100

    This is my first_project

---
+++ First_project.py
@@ -0,0 +1,14 @@
+def odd_even_project() :
+    number = input("Enter a number :")
+    while number != "x":
+
+        try:
+            number = int(number)
+            if number%2 == 0 :
+                print("It's an even number")
+            else :
+                print("It's an odd number")
+        except ValueError:
+            print("Please Enter a valid number")
+
+        number = input("Enter a number again , and if you wanna exit press 'x' :")
